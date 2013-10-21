caeec69e3407b85896b1bc02aea5aa721799b4d5
JJ Buckley <jj@bjjbuckley.com>
2013-10-21 02:02:16 +0200

    Enables rubygems completion

--- completion.bash
+++ completion.bash
@@ -1,8 +1,17 @@
 # Bash
-f=/usr/local/etc/bash_completion
-[ -r $f ] && . $f
+if [ -r /usr/local/etc/bash_completion ]
+then
+  . /usr/local/etc/bash_completion
+fi
 
 # Node, npm
-if [ -r /usr/local/lib/node_modules/npm/lib/utils/completion.sh ] ; then
+if [ -r /usr/local/lib/node_modules/npm/lib/utils/completion.sh ]
+then
   . /usr/local/lib/node_modules/npm/lib/utils/completion.sh
+fi
+
+# Rubygems
+if [ -r $HOME/.dotfiles/rubygems-completion.bash ]
+then
+  . $HOME/.dotfiles/rubygems-completion.bash
 fi
