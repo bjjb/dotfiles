1b17b28c3c3cfd443f71cfbb512e958dbd4b6f84
JJ Buckley <jj@bjjb.org>
2017-02-06 12:52:59 +0000

    Uses cloudflarer for a cfddns function

--- .bash/functions.bash
+++ .bash/functions.bash
@@ -15,3 +15,21 @@ digitalocean () {
     -H "Authorization: Bearer $DIGITALOCEAN_ACCESS_TOKEN" \
     "https://api.digitalocean.com/v2/$1" | jsonpp;
 }
+
+cfddns () {
+  name=${1:?'Usage: cfddns <fqdn>'} # 1st arg (required): the full domain name
+  zone=${1#*.} # the zone - everything after the first dot
+  zid=$(cloudflarer zones | grep "$zone" | cut -d' ' -f1) # the CF zone ID
+  newip=$(dig +short myip.opendns.com @resolver1.opendns.com) # new address
+  record=$(cloudflarer records -z $zid | grep $name) # existing CF record
+  oldip=${record##* } # existing address according to CF record
+  if [ "$oldip" != "$newip" ]; then # we need to update
+    echo "Old: ${oldip:-unset}: -> New: $newip"
+    if [ "$oldip" != "" ]; then # we need to delete the old record
+      rid=${record%% *} # the CF record ID
+      cloudflarer records delete -z $zid $rid
+    fi
+    # create a new record for the entry
+    cloudflarer records create -z $zid --name $name --type A --content $newip
+  fi
+}
