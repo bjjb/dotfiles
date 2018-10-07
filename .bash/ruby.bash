37dade54e68839c460f19958feb8e39ac8c3bd67
JJ Buckley <jj@bjjb.org>
2018-10-07 16:40:27 +0200

    Cleans up the ruby helpers

--- .bash/ruby.bash
+++ .bash/ruby.bash
@@ -4,3 +4,11 @@ chruby="/usr/local/share/chruby/chruby.sh"
 # Use the .ruby-version trick
 auto="/usr/local/share/chruby/auto.sh"
 [ -f "$auto" ] && . $auto
+
+rake () { [ -e bin/rake ] && bin/rake $@ || $(which -s rake) $@ ; }
+rails () { [ -e bin/rails ] && bin/rails $@ || $(which -s rails) $@ ; }
+rspec () { [ -e bin/rspec ] && bin/rspec $@ || $(which -s rspec) $@ ; }
+guard () { [ -e bin/guard ] && bin/guard $@ || $(which -s guard) $@ ; }
+rackup () { [ -e bin/rackup ] && bin/rackup $@ || $(which -s rackup) $@ ; }
+dotenv () { [ -e bin/dotenv ] && bin/dotenv $@ || $(which -s dotenv) $@ ; }
+rubocop () { [ -e bin/rubocop ] && bin/rubocop $@ || $(which -s rubocop) $@ ; }
