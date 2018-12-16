a22140c9db59a3e170c361975b23016754236a31
Bryan JJ Buckley <jj@bjjb.org>
2018-12-16 15:28:15 +0100

    Cleans up some bash functions

--- .bash/functions.bash
+++ .bash/functions.bash
@@ -1,31 +1,61 @@
 # Function definitions. Sourced by ~/.bash_profile
 
+# This insanely useful option ensures that the result of a pipe is the result
+# of the last failing command
+set -o pipefail
+
+# Check whether an executable can be found
+have() {
+  which $@ > /dev/null
+}
+
 # Music Player Client
-mp () { mpc toggle; }
+mp () { have mpc && mpc toggle; }
 
-g () { git $*; }
-d () { docker $*; }
-dm () { docker-machine $@; }
+# git aliases
+g () { have git && git $*; }
 
-tn () { tmux new-session -s ${1:-${PWD##*/}}; }
-ta () { tmux attach-session; }
-
+# docker aliases
+d () { have docker && docker $*; }
+dm () { have docker-machine && docker-machine $@; }
 devbox () { docker run --rm -it bjjb/devbox sh; }
 alpine () { docker run --rm -it bjjb/devbox:alpine ${*:-sh}; }
 ubuntu () { docker run --rm -it bjjb/devbox:ubuntu ${*:-bash}; }
 
+# tmux aliases
+tn () { tmux new-session -s ${1:-${PWD##*/}}; }
+ta () { tmux attach-session; }
+
+# Randomize the args
 shuffle () {
-  ruby -e 'puts ARGF.read.split("\n").reject(&:nil?).compact.shuffle.join("\n")'
+  have ruby || return "Missing ruby"
+  ruby -e <<EOF
+  puts ARGF.read.split("\n").reject(&:nil?).compact.shuffle.join("\n")
+EOF
 }
 
+# Simple digitalocean client
 digitalocean () {
+  have curl jsonpp || echo "Missing curl or jsonpp" && return
   curl -X GET                                             \
     -H "Content-Type: application/json"                   \
     -H "Authorization: Bearer $DIGITALOCEAN_ACCESS_TOKEN" \
     "https://api.digitalocean.com/v2/$1" | jsonpp;
 }
 
+# Gets the locally set IP address
+myipv6 () {
+  have ip || return "Missing ip"
+  ip -o -6 address \
+    | grep -v 'deprecated' \
+    | awk '/global/ { print $4 }' \
+    | cut -d '/' -f1 \
+    | head -1;
+}
+
+# Simple cloudflare DDNS client
 cfddns () {
+  have cloudflarer || echo "Missing cloudflarer" && return
   name=${1:?'Usage: cfddns <fqdn>'} # 1st arg (required): the full domain name
   zone=${1#*.} # the zone - everything after the first dot
   zid=$(cloudflarer zones | grep "$zone" | cut -d' ' -f1) # the CF zone ID
@@ -43,4 +73,5 @@ cfddns () {
   fi
 }
 
+# Source local functions also, if present.
 [[ -x "$HOME/.functions.bash" ]] && . "$HOME/.functions.bash"
