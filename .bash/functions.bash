c686f8a288eba061ea0b1ea14cb7450325d304ab
Bryan JJ Buckley <jj@bjjb.org>
2019-02-12 13:01:19 +0100

    Better CloudFlare DDNS
    Now it reads the list of domains from a YAML file in your home
    directory, and creates/updates the records as needed.

--- .bash/functions.bash
+++ .bash/functions.bash
@@ -60,24 +60,12 @@ myipv6 () {
     | head -1;
 }
 
-# Simple cloudflare DDNS client
-cfddns () {
-  have cloudflarer || echo "Missing cloudflarer" && return
-  name=${1:?'Usage: cfddns <fqdn>'} # 1st arg (required): the full domain name
-  zone=${1#*.} # the zone - everything after the first dot
-  zid=$(cloudflarer zones | grep "$zone" | cut -d' ' -f1) # the CF zone ID
-  newip=$(dig +short myip.opendns.com @resolver1.opendns.com) # new address
-  record=$(cloudflarer records -z $zid | grep $name) # existing CF record
-  oldip=${record##* } # existing address according to CF record
-  if [ "$oldip" != "$newip" ]; then # we need to update
-    echo "Old: ${oldip:-unset}: -> New: $newip"
-    if [ "$oldip" != "" ]; then # we need to delete the old record
-      rid=${record%% *} # the CF record ID
-      cloudflarer records delete -z $zid $rid
-    fi
-    # create a new record for the entry
-    cloudflarer records create -z $zid --name $name --type A --content $newip
-  fi
+# Use imagemagick to remove a white background from an image
+transparentize () {
+  have convert || echo "Missing imagemagick (convert)"
+  input=${1:?'Usage: transparentize <input> <output>'}
+  output=${2:?'Usage: transparentize <input> <output>'}
+  convert "$input" -fill none -draw "color 1,1 floodfill" "$output"
 }
 
 # Source local functions also, if present.
