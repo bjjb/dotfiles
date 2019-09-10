cb41add6baed8935048d7fc2999f70067be26036
JJ Buckley <10744+bjjb@users.noreply.github.com>
2019-09-10 19:27:25 +0200

    Modifies how we manage python environments
    Python3's venv is better.

--- .bash/python.bash
+++ .bash/python.bash
@@ -1,7 +1,13 @@
-if [ -d "$HOME/.pyenv/bin" ]
-then
-  echo "Setting up pyenv"
-  PATH="$PATH:$HOME/.pyenv/bin"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
+venv() {
+  # shellcheck source=/dev/null
+  venv="${HOME}/.venv"
+  if ! [ -d "${venv}" ]
+  then
+    python3 -m venv "${venv}"
+  fi
+  # shellcheck source=/dev/null
+  . "${venv}/bin/activate"
+}
+
+# Activate a python3 virtual environment (if python3 is installed).
+which python3 > /dev/null && venv
